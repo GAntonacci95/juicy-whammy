3f0196fbee81a9c806d8aa1e24f5e5541e32952c
Gabriele Antonacci <>
2020-06-17 18:00:17 +0200

    prova non testata

---
+++ WhammyPlug/Builds/MacOSX/ChannelThread.cpp
@@ -0,0 +1,8 @@
+//
+//  ChannelThread.cpp
+//  WhammyPlug - All
+//
+//  Created by Gabriele Antonacci on 17/06/2020.
+//
+
+#include <stdio.h>
---
+++ WhammyPlug/Builds/MacOSX/ChannelThread.h
@@ -0,0 +1,12 @@
+//
+//  ChannelThread.h
+//  WhammyPlug - All
+//
+//  Created by Gabriele Antonacci on 17/06/2020.
+//
+
+#ifndef ChannelThread_h
+#define ChannelThread_h
+
+
+#endif /* ChannelThread_h */
