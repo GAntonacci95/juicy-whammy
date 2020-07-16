fe830d98e2c25c847cbb7b3321f12cb1b8777af8
Gabriele Antonacci <>
2020-07-16 22:07:46 +0200

    lilarray

---
+++ WhammyPlug/Source/LilArray.cpp
@@ -0,0 +1,8 @@
+//
+//  LilArray.cpp
+//  WhammyPlug - Shared Code
+//
+//  Created by Gabriele Antonacci on 16/07/2020.
+//
+
+#include "LilArray.hpp"
---
+++ WhammyPlug/Source/LilArray.h
@@ -0,0 +1,13 @@
+//
+//  LilArray.hpp
+//  WhammyPlug - Shared Code
+//
+//  Created by Gabriele Antonacci on 16/07/2020.
+//
+
+#ifndef LilArray_hpp
+#define LilArray_hpp
+
+#include <stdio.h>
+
+#endif /* LilArray_hpp */
